0abaf2677ac05336fa30c7dc0b8ab85bad377e39
Andre Talpo <andre.talpo@gmail.com>
2019-11-17 21:10:46 -0300

    Versao inicial

---
+++ src/main/java/br/com/andre/anubis/AnubisApplication.java
@@ -0,0 +1,13 @@
+package br.com.andre.anubis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnubisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnubisApplication.class, args);
+	}
+
+}
